d635b761846e5a92c2b8699680c146d4e008f239
ax2503 <ax2503@gmail.com>
2018-12-21 14:08:56 +1000

    21/12/2018

--- statistical_queries.py
+++ statistical_queries.py
@@ -9,3 +9,6 @@ def TopRange(tradedate, table, limit) :
     sql = ('SELECT stock_code, (high - low)/100 as gain FROM ' + table + ' WHERE trade_date = ' + tradedate + 
         ' ORDER BY gain DESC LIMIT ' + limit + ' ;')
     return sql
+
+def Rises(traddate, table) :
+    ##Number of rising issues (Close - Open) > 0
